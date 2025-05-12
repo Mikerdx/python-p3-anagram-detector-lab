492d2b173a8d5d3921b49103977ea41b6e5c2fa5
Mike Bett <mike.bett@student.moringa.school.com>
2025-05-12 12:56:23 +0300

    done

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,12 @@
 # your code goes here!
+class Anagram:
+    def __init__(self, word):
+        self.word = word.lower()
+
+    def match(self, word_list):
+        sorted_word = sorted(self.word)
+        matches = []
+        for candidate in word_list:
+            if sorted(candidate.lower()) == sorted_word:
+                matches.append(candidate)
+        return matches
